f05b180364036227617e31de68a34b9b47c82f27
Alisa <alisa.melissa@icloud.com>
2023-12-04 16:56:17 +0200

    Initial Commit

---
+++ ButtonReel/ButtonReelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonReelApp.swift
+//  ButtonReel
+//
+//  Created by Алиса Третьякова on 04.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ButtonReelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
